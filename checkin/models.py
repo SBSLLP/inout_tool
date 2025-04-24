3b1141ff2f32944458c89d8494a63f117c4d75e6
hariprasand066 <harikanagaraj18052002@gmail.com>
2025-04-25 00:37:33 +0530

    Your meaningful commit message

--- checkin/models.py
+++ checkin/models.py
@@ -4,19 +4,23 @@ from django.contrib.auth.models import AbstractUser,BaseUserManager, Permissions
 from django.conf import settings
 
 class CustomUserManager(BaseUserManager):
-        def create_user(self, email, password=None, role=None, **extra_fields):
-            if not email:
-                raise ValueError('Email address is required')
-            email = self.normalize_email(email)
-            user = self.model(email=email, role=role, **extra_fields)
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
+    def create_user(self, email, password=None, role=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not role:
+            raise ValueError("Users must have a role")
 
-        def create_superuser(self, email, password=None, role=None, **extra_fields):
-            extra_fields.setdefault('is_staff', True)
-            extra_fields.setdefault('is_superuser', True)
-            return self.create_user(email, password, role=role, **extra_fields)
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, role='SuperAdmin', **extra_fields)
+
       
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
